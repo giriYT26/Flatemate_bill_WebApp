353ebbed21a04546d9e01216c84a7d5700ce4c78
giriYT26 <srikalgiri@gmail.com>
2025-09-16 19:37:10 +0530

    feat:Implemtation of Resultpage and submit button in billfrom page

--- FlatemateBill_V01.py
+++ FlatemateBill_V01.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask.views import MethodView
 from wtforms import Form,StringField,SubmitField
 
@@ -7,12 +7,19 @@ app = Flask(__name__)
 class Homepage(MethodView):
     def get(self):
         return render_template("index.html")
+    
 class BillFormPage(MethodView):
     def get(self):
         bill_form = BillForm()
         return render_template("bill_form_page.html",billform = bill_form)
+    
 class ResultPage(MethodView):
-    pass    
+    def post(self):
+        billform = BillForm(request.form)
+        bill_amount = billform.bill_amt.data
+        bill_period = billform.bill_period
+        return bill_amount,bill_period
+    
 class BillForm(Form):
     bill_amt = StringField(label="Bill Amount:")
     bill_period = StringField(label="Bill Period:")
@@ -34,6 +41,7 @@ The as_view("func_name_you_want") converts the class into a view function, and "
 
 app.add_url_rule("/",view_func=Homepage.as_view("Home_page")) 
 app.add_url_rule("/bill",view_func=BillFormPage.as_view("Bill_Form_Page"))
+app.add_url_rule("/results",view_func=ResultPage.as_view("Results_page"))
 
 app.run(debug=True)
 
